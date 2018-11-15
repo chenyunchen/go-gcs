3945435b28f62d04c077110fe0fd51794a9d20d0
Chiahsun Cheng <chiahsun.cheng@jkopay.com>
2018-11-15 17:48:15 +0800

    Deploy to develop

--- deployment/ansible.mk
+++ deployment/ansible.mk
@@ -8,6 +8,9 @@ common-stop:
 common-check:
 	ansible-playbook -i ../../environments/$(ENV) check.yml
 
+common-ping:
+	ansible -i ../../environments/$(ENV) all -m ping
+
 ifeq ($(ENV),)
 $(error "Usage: make $(MAKECMDGOALS) ENV=local")
 endif
